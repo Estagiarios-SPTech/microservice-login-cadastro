825d9eff91b585d08cfecc180b314cd62cd6f774
Shirley Ferreira Lima <sflima2@stefanini.com>
2025-04-30 14:32:59 -0300

    atualização do script

--- scriptBanco.sql
+++ scriptBanco.sql
@@ -4,8 +4,24 @@ use ordensof;
 
 create table users (
 	id int primary key auto_increment,
-    name varchar(120),
+    `name` varchar(120),
     email varchar(120),
-    role varchar(50),
-    password varchar(35)
+    `role` varchar(50),
+    `password` varchar(35)
 );
+
+create table employees (
+	id int primary key auto_increment,
+    `user` int, 
+    rt int, 
+    manager int,
+    `status` varchar(30)
+);
+
+create table ordensFornecimento (
+	codigo int primary key auto_increment,
+	`description` varchar(250),
+	`status` varchar(30),
+	created_at datetime, 
+	updated_at datetime
+)
